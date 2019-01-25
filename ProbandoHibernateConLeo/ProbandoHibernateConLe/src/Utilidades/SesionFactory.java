8bb93a13109b58c4cbd878c7cab21f26c0ff14fc
RafaMateos <rafaelmateostorres@gmail.com>
2019-01-25 14:19:44 +0100

    Hibernate funciona

---
+++ ProbandoHibernateConLeo/ProbandoHibernateConLe/src/Utilidades/SesionFactory.java
@@ -0,0 +1,38 @@
+
+package probandohibernate;
+
+/**
+ *
+ * @author leo
+ */
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class SesionFactory {
+    private static ServiceRegistry serviceRegistry;
+    private static final SessionFactory sessionFactory;
+ 
+    static {
+        try {
+Configuration configuration=new Configuration()
+            .configure(); // configures settings from hibernate.cfg.xml
+
+    StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
+
+    // If you miss the below line then it will complaing about a missing dialect setting
+    serviceRegistryBuilder.applySettings(configuration.getProperties());
+
+    serviceRegistry = serviceRegistryBuilder.build();
+    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+ 
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
--- ProbandoHibernateConLeo/ProbandoHibernateConLe/src/probandohibernateconle/ProbandoHibernateConLe.java
+++ ProbandoHibernateConLeo/ProbandoHibernateConLe/src/probandohibernateconle/ProbandoHibernateConLe.java
@@ -5,6 +5,12 @@
  */
 package probandohibernateconle;
 
+import modelos.Criaturita;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import probandohibernate.SesionFactory;
+
 /**
  *
  * @author rmateos
@@ -14,8 +20,46 @@ public class ProbandoHibernateConLe {
     /**
      * @param args the command line arguments
      */
+    private static SessionFactory sessionFactory = null;
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Session session = null;
+        sessionFactory = SesionFactory.getSessionFactory();
+        session = sessionFactory.openSession();
+       
+        Transaction tx = session.beginTransaction();
+        Criaturita criatura = new Criaturita();
+        
+        byte id = 71;
+        byte id2 = 2;
+        
+        System.out.println("Guardamos a Juan con id "+ id);
+        criatura.setId(id);
+        criatura.setNombre("Juan");
+        session.save(criatura);
+        
+         System.out.println("Recojemos un registro(Adelita)");
+        Criaturita nene = (Criaturita)session.get(Criaturita.class, id2);
+        System.out.println(nene.getNombre());
+        
+        System.out.println("Le cambiamos el nombre a Adelita(LeoApruebame)");
+        nene.setNombre("LeoApruebame");
+        
+        System.out.println("Borramos a Juan con id 70");
+        id2 = 70;
+        Criaturita juan = (Criaturita)session.get(Criaturita.class, id2);
+        session.delete(juan);
+        
+        tx.commit();
+        session.close();
+        
+        
+        
+       
+        
+        
+        
     }
     
 }
